47f1937c917418bc88ced944bb201164ed9e3546
prioarief <prioariefgunawangunawan@gmail.com>
2021-01-18 15:52:48 +0700

    add timer countdown

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,51 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Head from "next/head";
+import React from 'react'
+
 
 export default function Home() {
+
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    const difference = +new Date(`${year}-10-1`) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = React.useState(calculateTimeLeft());
+  const [year] = React.useState(new Date().getFullYear());
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+
   return (
     <div
       className="d-flex flex-column justify-content-center align-items-center"
@@ -9,7 +53,7 @@ export default function Home() {
     >
       <Head>
         <title>Hello World</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://avatars2.githubusercontent.com/u/49262552?s=460&u=fb8ec315faff9451a8701db4f6abbb50ed12d4b2&v=4" className='rounded-pill' />
       </Head>
 
       <img
@@ -18,6 +62,7 @@ export default function Home() {
       />
       <h5 className="mt-5">Website Under Maintenance</h5>
       <h5 className="">We'll Back Soon</h5>
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
     </div>
   );
 }

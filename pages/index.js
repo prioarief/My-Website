7282904c40d4ed7554a981eb5d21963e5a339e0a
prioarief <prioariefgunawangunawan@gmail.com>
2021-01-18 15:39:58 +0700

    add styling landing page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,23 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
       <Head>
         <title>Hello World</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <img src='http://stikesmerangin.ac.id/maintenance-final.gif' className='img-fluid' />
-      <h3 className='mt-5'>Website Under Maintenance</h3>
-      <h3 className=''>We'll Back Soon</h3>
+      <img
+        src="http://stikesmerangin.ac.id/maintenance-final.gif"
+        className="img-fluid px-4"
+      />
+      <h5 className="mt-5">Website Under Maintenance</h5>
+      <h5 className="">We'll Back Soon</h5>
     </div>
   );
 }
